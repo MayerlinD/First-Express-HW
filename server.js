a7db67595d1038e2b4ed3b526d24c9fef45b368e
Mayerlin Duluc <may_duluc@hotmail.com>
2022-09-28 13:40:43 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>Greetings ${user}</h1>`);
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const ans = (parseInt(req.params.tipPercentage) / 100) * parseInt(req.params.total)
+    res.send(`<h1>Your tip amount is ${ans}</h1>`);
+});
+
+
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
  console.log('Listening on port 3000');
