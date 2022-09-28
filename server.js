409d127a3f8169ccf0f36225db9cde1cfb59d57f
Mayerlin Duluc <may_duluc@hotmail.com>
2022-09-28 12:50:49 -0400

    Greetings completed

---
+++ server.js
@@ -0,0 +1,26 @@
+// Require modules
+const express = require('express');
+
+// Create the Express app
+const app = express();
+
+// Configure the app (app.set)
+
+
+// Mount middleware (app.use)
+
+
+// Mount routes
+app.get('/', (req, res) => {
+  	res.send('<h1>Hello World</h1>');
+});
+
+app.get('/greeting/:name', (req, res) => {
+    const user = req.params.name
+    res.send(`<h1>Greetings ${user}</h1>`);
+});
+
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+ console.log('Listening on port 3000');
+});
